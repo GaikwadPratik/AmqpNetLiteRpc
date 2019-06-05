a82b90085e49870dfde9add95412f0c896ef6910
Pratik Gaikwad <noreplyfrompratik@gmail.com>
2019-06-04 22:00:04 -0400

    refactor(utility): added `TimeoutAfter` extension method for `Task<T>` and moved GetRequestMessage here as it is to be used in both client and server

--- AmqpNetLiteRpcCore/Util/Utility.cs
+++ AmqpNetLiteRpcCore/Util/Utility.cs
@@ -1,3 +1,12 @@
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Amqp;
+using Amqp.Serialization;
+using Serilog;
+
 namespace AmqpNetLiteRpcCore
 {
     public class AmqpRpcNode
@@ -25,6 +34,68 @@ namespace AmqpNetLiteRpcCore
             result.Address = tempAddress[0];
             result.Subject = tempAddress[1];
             return result;
+        }
+
+        /// <summary>
+        /// Converts parameter object from Amqp request to a particular type to be used as input parameter to RPC method being invoked
+        /// </summary>
+        /// <param name="deserializationType">Type into which object parameters must be converted</param>
+        /// <param name="parameters">Input received at AmqpRequest.body.params</param>
+        /// <returns>Deserialized object which will be used as an input to Rpc Method</returns>
+        public dynamic GetRequestMessage(Type deserializationType, object parameters)
+        {
+            try
+            {
+                if (deserializationType == null)
+                {
+                    return null;
+                }
+                //Create Amqp serializer instance
+                AmqpSerializer _serializer = new AmqpSerializer();
+                //Create dynamic buffer
+                ByteBuffer _paramsBuffer = new ByteBuffer(1024, true);
+                //Write object to buffer
+                _serializer.WriteObject(_paramsBuffer, parameters);
+                //Get ReadObject methodinfo using reflection 
+                var _readObjectMethodInfo = typeof(AmqpSerializer).GetMethods(BindingFlags.Public | BindingFlags.Instance)
+                    .Where(x => x.Name.Equals("ReadObject") && x.IsGenericMethod && x.GetGenericArguments().Length.Equals(1))
+                    .FirstOrDefault();
+                if (_readObjectMethodInfo == null)
+                {
+                    throw new MissingMethodException("ReadObject from AmqpSerializer");
+                }
+                //Mark methodinfo as generic
+                var _readObjectGenericMethodInfo = _readObjectMethodInfo.MakeGenericMethod(deserializationType);
+                //Invoke ReadObject to deserialize object
+                return _readObjectGenericMethodInfo.Invoke(_serializer, new[] { _paramsBuffer });
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Exception while deserializing request parameters", parameters);
+            }
+            return null;
+        }
+    }
+
+    public static class Extensions
+    {
+        public static async Task<TResult> TimeoutAfter<TResult>(this Task<TResult> task, int timeout)
+        {
+
+            using (var timeoutCancellationTokenSource = new CancellationTokenSource())
+            {
+
+                var completedTask = await Task.WhenAny(task, Task.Delay(timeout, timeoutCancellationTokenSource.Token));
+                if (completedTask == task)
+                {
+                    timeoutCancellationTokenSource.Cancel();
+                    return await task;  // Very important in order to propagate exceptions
+                }
+                else
+                {
+                    throw new TimeoutException();
+                }
+            }
         }
     }
 }
