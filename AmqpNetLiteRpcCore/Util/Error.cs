12e020318f3c45e866926a71dceaf8f1cb480536
Pratik Gaikwad <noreplyfrompratik@gmail.com>
2019-06-04 21:57:18 -0400

    refactor(Error): added custom rpcexception to be travesed over qpid and thrown from client

--- AmqpNetLiteRpcCore/Util/Error.cs
+++ AmqpNetLiteRpcCore/Util/Error.cs
@@ -3,20 +3,46 @@ using Amqp.Serialization;
 
 namespace AmqpNetLiteRpcCore
 {
-    [AmqpContract]
-    public class AmqpRpcResponseException : Exception
+    public class AmqpRpcException : Exception
     {
-        [AmqpMember (Name = "code")]
-        public string Code { get; set; }
-        public AmqpRpcResponseException(string message = "") : base(message)
+        private string _stackTrace = string.Empty;
+        private string _code = string.Empty;
+        public string Code
         {
+            get
+            {
+                return this._code;
+            }
         }
+        public override string StackTrace
+        {
+            get
+            {
+                return this._stackTrace;
+            }
+        }
+        public AmqpRpcException(string message = "", string stackTrace = null, string code = null) : base(message)
+        {
+            this._stackTrace = stackTrace;
+            this._code = code;
+        }
+    }
+
+    [AmqpContract(Encoding = EncodingType.SimpleMap)]
+    public class AmqpRpcServerException
+    {
+        [AmqpMember(Name = "code")]
+        public string Code { get; set; }
+        [AmqpMember(Name = "message")]
+        public string Message { get; set; }
+        [AmqpMember(Name = "stack")]
+        public string Stack { get; set; }
     }
 
     [AmqpContract]
     public class AmqpRpcRequestTimeoutException : Exception
     {
-        [AmqpMember (Name = "code")]
+        [AmqpMember(Name = "code")]
         public string Code = ErrorCode.AmqpRpcRequestTimeOut;
         public AmqpRpcRequestTimeoutException(string message) : base(message)
         {
@@ -26,7 +52,7 @@ namespace AmqpNetLiteRpcCore
     [AmqpContract]
     public class AmqpRpcMissingFunctionDefinitionException : Exception
     {
-        [AmqpMember (Name = "code")]
+        [AmqpMember(Name = "code")]
         public string Code = ErrorCode.AmqpRpcMissingFunctionDefinition;
         public AmqpRpcMissingFunctionDefinitionException(string message) : base(message)
         {
@@ -36,7 +62,7 @@ namespace AmqpNetLiteRpcCore
     [AmqpContract]
     public class AmqpRpcMissingFunctionNameException : Exception
     {
-        [AmqpMember (Name = "code")]
+        [AmqpMember(Name = "code")]
         public string Code = ErrorCode.AmqpRpcMissingFunctionName;
         public AmqpRpcMissingFunctionNameException(string message) : base(message)
         {
@@ -46,7 +72,7 @@ namespace AmqpNetLiteRpcCore
     [AmqpContract]
     public class AmqpRpcDuplicateFunctionDefinitionException : Exception
     {
-        [AmqpMember (Name = "code")]
+        [AmqpMember(Name = "code")]
         public string Code = ErrorCode.AmqpRpcDuplicateFunctionDefinition;
         public AmqpRpcDuplicateFunctionDefinitionException(string message) : base(message)
         {
@@ -56,7 +82,7 @@ namespace AmqpNetLiteRpcCore
     [AmqpContract]
     public class AmqpRpcParamsNotObjectException : Exception
     {
-        [AmqpMember (Name = "code")]
+        [AmqpMember(Name = "code")]
         public string Code = ErrorCode.AmqpRpcParamsNotObject;
         public AmqpRpcParamsNotObjectException(string message) : base(message)
         {
@@ -66,7 +92,7 @@ namespace AmqpNetLiteRpcCore
     [AmqpContract]
     public class AmqpRpcParamsMissingPropertiesException : Exception
     {
-        [AmqpMember (Name = "code")]
+        [AmqpMember(Name = "code")]
         public string Code = ErrorCode.AmqpRpcParamsMissingProperties;
         public AmqpRpcParamsMissingPropertiesException(string message) : base(message)
         {
@@ -86,7 +112,7 @@ namespace AmqpNetLiteRpcCore
     [AmqpContract]
     public class AmqpRpcUnknownFunctionException : Exception
     {
-        [AmqpMember (Name = "code")]
+        [AmqpMember(Name = "code")]
         public string Code = ErrorCode.AmqpRpcUnknownFunction;
         public AmqpRpcUnknownFunctionException(string message) : base(message)
         {
@@ -96,7 +122,7 @@ namespace AmqpNetLiteRpcCore
     [AmqpContract]
     public class AmqpRpcFunctionDefinitionValidationException : Exception
     {
-        [AmqpMember (Name = "code")]
+        [AmqpMember(Name = "code")]
         public string Code = ErrorCode.AmqpRpcFunctionDefinitionValidationError;
         public AmqpRpcFunctionDefinitionValidationException(string message) : base(message)
         {
@@ -106,7 +132,7 @@ namespace AmqpNetLiteRpcCore
     [AmqpContract]
     public class AmqpRpcInvalidNodeAddressException : Exception
     {
-        [AmqpMember (Name = "code")]
+        [AmqpMember(Name = "code")]
         public string Code = ErrorCode.AmqpRpcInvalidNodeAddressException;
         public AmqpRpcInvalidNodeAddressException(string message) : base(message)
         {
@@ -116,7 +142,7 @@ namespace AmqpNetLiteRpcCore
     [AmqpContract]
     public class AmqpRpcInvalidRpcTypeException : Exception
     {
-        [AmqpMember (Name = "code")]
+        [AmqpMember(Name = "code")]
         public string Code = ErrorCode.AmqpRpcInvalidRpcTypeException;
         public AmqpRpcInvalidRpcTypeException(string message) : base(message)
         {
